5d73c80841723ee1730912961d6bb1c3015020ed
shams <mdshamsalsiam7@gmail.com>
2025-03-02 11:59:10 +0600

    first commit

---
+++ alljs/main.js
@@ -0,0 +1,181 @@
+document.getElementById("btn1").addEventListener("click", function() {
+
+    this.disabled = true;
+
+   const currentTime = new Date().toLocaleTimeString(); 
+   document.getElementById("mag").innerText = "You have completed the task! Added Dark Mode at " + currentTime;
+
+   let tw = parseInt(document.getElementById("task").innerText);
+   let number = parseInt(document.getElementById("tow").innerText);
+
+   if (number > 0) { 
+       number++;  
+       document.getElementById("tow").innerText = number;
+       alert ("your are done thik task");  
+
+       if (tw > 0) {
+           tw--;  
+           document.getElementById("task").innerText = tw;
+       } else {
+           alert("Hoba nah!");
+       }
+   } else {
+       alert("Number 0 er niche nama jabe na!");
+   }
+});
+
+
+document.getElementById("btn2").addEventListener("click", function() {
+
+    this.disabled = true;
+
+   const currentTime = new Date().toLocaleTimeString(); 
+   document.getElementById("mag2").innerText = "You have completed the task! Added Dark Mode at " + currentTime;
+
+   let tw = parseInt(document.getElementById("task").innerText);
+   let number = parseInt(document.getElementById("tow").innerText);
+
+   if (number > 0) { 
+       number++;  
+       document.getElementById("tow").innerText = number;
+       alert ("your are done thik task");  
+
+       if (tw > 0) {
+           tw--;  
+           document.getElementById("task").innerText = tw;
+       } else {
+           alert("Hoba nah!");
+       }
+   } else {
+       alert("Number 0 er niche nama jabe na!");
+   }
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+
+    this.disabled = true;
+
+   const currentTime = new Date().toLocaleTimeString(); 
+   document.getElementById("mag3").innerText = "You have completed the task! Added Dark Mode at " + currentTime;
+
+   let tw = parseInt(document.getElementById("task").innerText);
+   let number = parseInt(document.getElementById("tow").innerText);
+
+   if (number > 0) { 
+       number++;  
+       document.getElementById("tow").innerText = number; 
+
+       if (tw > 0) {
+           tw--;  
+           document.getElementById("task").innerText = tw;
+           alert ("your are done thik task"); 
+       } else {
+           alert("Hoba nah!");
+       }
+   } else {
+       alert("Number 0 er niche nama jabe na!");
+   }
+});
+
+
+document.getElementById("btn4").addEventListener("click", function() {
+
+    this.disabled = true;
+
+   const currentTime = new Date().toLocaleTimeString(); 
+   document.getElementById("mag4").innerText = "You have completed the task! Added Dark Mode at " + currentTime;
+
+   let tw = parseInt(document.getElementById("task").innerText);
+   let number = parseInt(document.getElementById("tow").innerText);
+
+   if (number > 0) { 
+       number++;  
+       document.getElementById("tow").innerText = number; 
+
+       if (tw > 0) {
+           tw--;  
+           document.getElementById("task").innerText = tw;
+           alert ("your are done thik task"); 
+       } else {
+           alert("Hoba nah!");
+       }
+   } else {
+       alert("Number 0 er niche nama jabe na!");
+   }
+});
+
+
+document.getElementById("btn5").addEventListener("click", function() {
+
+    this.disabled = true;
+
+   const currentTime = new Date().toLocaleTimeString(); 
+   document.getElementById("mag5").innerText = "You have completed the task! Added Dark Mode at " + currentTime;
+
+   let tw = parseInt(document.getElementById("task").innerText);
+   let number = parseInt(document.getElementById("tow").innerText);
+
+   if (number > 0) { 
+       number++;  
+       document.getElementById("tow").innerText = number; 
+       alert ("your are done thik task"); 
+
+       if (tw > 0) {
+           tw--;  
+           document.getElementById("task").innerText = tw;
+       } else {
+           alert("Hoba nah!");
+       }
+   } else {
+       alert("Number 0 er niche nama jabe na!");
+   }
+});
+
+
+document.getElementById("btn6").addEventListener("click", function() {
+
+    this.disabled = true;
+
+   const currentTime = new Date().toLocaleTimeString(); 
+   document.getElementById("mag6").innerText = "You have completed the task! Added Dark Mode at " + currentTime;
+
+   let tw = parseInt(document.getElementById("task").innerText);
+   let number = parseInt(document.getElementById("tow").innerText);
+
+   if (number > 0) { 
+       number++;  
+       document.getElementById("tow").innerText = number;
+       
+
+       if (tw > 1) {
+           tw--;  
+           document.getElementById("task").innerText = tw;
+       } else {
+           alert("Hoba nah!");
+       }
+   } else {
+       alert("Number 0 er niche nama jabe na!");
+   }
+});
+
+
+
+document.getElementById("Clear").addEventListener("click", function() {
+    
+    document.getElementById("mag")?.remove();
+    document.getElementById("mag2")?.remove();
+    document.getElementById("mag3")?.remove();
+    document.getElementById("mag4")?.remove();
+    document.getElementById("mag5")?.remove();
+    document.getElementById("mag6")?.remove();
+});
+
+
+
+document.getElementById("btn-color").addEventListener("click", function() {
+    
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    
+    document.body.style.backgroundColor = randomColor;
+});
---
+++ alljs/next.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("next").addEventListener("click", function() {
+        window.location.href = "next.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("back").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
